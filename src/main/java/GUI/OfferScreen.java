2c605e22f1480d5dcb0d1d70fa6f0e44881ac76f
Gabriela Topczewska <01158963@pw.edu.pl>
2021-11-26 19:10:59 +0100

    offer screen designed

---
+++ src/main/java/GUI/OfferScreen.java
@@ -0,0 +1,12 @@
+package GUI;
+
+import Classes.*;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class OfferScreen {
+}
