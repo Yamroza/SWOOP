de9490773d61372542d0888e2a08a6c5dfa2c121
Aleksandra Jamróz <01158705@pw.edu.pl>
2021-11-29 19:19:56 +0100

    Deleted useless functions from transactions array

--- src/main/java/Database/Transactions.java
+++ src/main/java/Database/Transactions.java
@@ -10,30 +10,16 @@ import java.util.Objects;
 public class Transactions {
 
     static ArrayList<Transaction> transactionsList = new ArrayList<>();
-    static Transaction completedTransaction;
 
-    public static ArrayList<Transaction> getUsersList() {
+    public static ArrayList<Transaction> getTransactionsList() {
         return transactionsList;
     }
-
-   /* public static User checkIfUserIsInBase(String givenUsername) {
-        for (User user : usersList) {
-            if (Objects.equals(user.getUsername(), givenUsername)) {
-                return user;
-            }
-        }
-        return null;
-    } */
 
     public static void addTransaction(User seller, Offer offer, User buyer, Offer buyersOffer) {
         transactionsList.add(new Transaction(seller, offer, buyer, buyersOffer));
     }
 
-    public static void setCompletedTransaction(Transaction transaction) {
-        completedTransaction = transaction;
-    }
+    /*
+    * */
 
-    public static Transaction getCompletedTransaction() {
-        return completedTransaction;
-    }
 }
