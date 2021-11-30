5b1db4b4718c2c5ff1c77e1031668e84844596e8
Aleksandra Jamróz <01158705@pw.edu.pl>
2021-11-30 21:35:45 +0100

    Functions corrected to show logs on screen

--- src/main/java/Database/Connecting.java
+++ src/main/java/Database/Connecting.java
@@ -4,32 +4,28 @@ import org.slf4j.LoggerFactory;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.*;
 
 
 public class Connecting {
 
-    // pola
     private static final Logger log = LoggerFactory.getLogger(Connecting.class);
     private Connection conn;
-    private static String url = "jdbc:oracle:thin:@//ora4.ii.pw.edu.pl:1521/pdb1.ii.pw.edu.pl";
+    private static final String url = "jdbc:oracle:thin:@//ora4.ii.pw.edu.pl:1521/pdb1.ii.pw.edu.pl";
     private static String username = "z27";
     private static String password = "9wdzsz";
 
-    // konstruktory
     public Connecting() throws SQLException {
         log.info("Opening connection to najlepszyzespol database");
         conn = DriverManager.getConnection(url, username, password);
     }
 
     public Connecting(String username, String password) throws SQLException {
-        this.username = username;
-        this.password = password;
-//        log.info("Opening connection to najlepszyzespol database");
-        conn = DriverManager.getConnection(url, this.username, this.password);
+        Connecting.username = username;
+        Connecting.password = password;
+        log.info("Opening connection to najlepszyzespol database");
+        conn = DriverManager.getConnection(url, Connecting.username, Connecting.password);
     }
 
-    // getter i setter
     public Connection getConn() {
         return conn;
     }
@@ -38,7 +34,6 @@ public class Connecting {
         this.conn = conn;
     }
 
-    // zamykanie bazy
     public void close() {
         if (conn != null) {
             try {
@@ -49,13 +44,6 @@ public class Connecting {
             }
             conn = null;
         }
-    }
-
-    // zwraca cos co ja nie wiem czym jest
-    public ResultSet query(String query) throws SQLException
-    {
-        Statement stmt = conn.createStatement();
-        return stmt.executeQuery(query);
     }
 }
 
