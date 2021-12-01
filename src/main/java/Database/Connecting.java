17f5abff4d040690b42501914d6625bdd17e8d34
Aleksandra Jamróz <01158705@pw.edu.pl>
2021-12-01 16:23:40 +0100

    Function to alter table

--- src/main/java/Database/Connecting.java
+++ src/main/java/Database/Connecting.java
@@ -16,6 +16,7 @@ public class Connecting {
     private static final String url = "jdbc:oracle:thin:@//ora4.ii.pw.edu.pl:1521/pdb1.ii.pw.edu.pl";
     private static String username = "z27";
     private static String password = "9wdzsz";
+    private String sql_instr = null;
 
     public Connecting() throws SQLException {
         log.info("Opening connection to najlepszyzespol database");
@@ -45,27 +46,28 @@ public class Connecting {
         }
     }
 
-    /*public void addAccount(int id, String username, String password, LocalDate creation_time){
-
-
-        }*/
-    public void addTable() throws SQLException {
+    public void alterTable(String sql_in) {
         Statement stmt = null;
         try {
             stmt = conn.createStatement();
-            stmt.execute("Drop TABLE TRANSACTIONS");
-            log.info("Droping table");
+            stmt.execute(sql_in);
+            log.info("Executing request");
         } catch (SQLException e) {
-            log.error("Unable to drop the database table", e);
+            log.error("Unable to execute request", e);
         } finally {
             if (stmt != null) {
                 try {
                     stmt.close();
-                } catch (SQLException e) {
+                } catch (SQLException ignored) {
                 }
             }
         }
     }
+
+    /*public void addAccount(int id, String username, String password, LocalDate creation_time){
+        Connecting.altertable
+    }
+    */
 }
 
 
