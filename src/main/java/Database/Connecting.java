535ef3e4a92969f3847a2f01b3da168cbcabcaa0
Aleksandra Jamróz <01158705@pw.edu.pl>
2021-12-01 19:30:07 +0100

    Added select * from users function

--- src/main/java/Database/Connecting.java
+++ src/main/java/Database/Connecting.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.*;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,20 +15,20 @@ public class Connecting {
     private static final Logger log = LoggerFactory.getLogger(Connecting.class);
     private Connection conn;
     private static final String url = "jdbc:oracle:thin:@//ora4.ii.pw.edu.pl:1521/pdb1.ii.pw.edu.pl";
-    private static String username = "z27";
+    private static String login = "z27";
     private static String password = "9wdzsz";
     private String sql_instr = null;
 
     public Connecting() throws SQLException {
         log.info("Opening connection to najlepszyzespol database");
-        conn = DriverManager.getConnection(url, username, password);
+        conn = DriverManager.getConnection(url, login, password);
     }
 
-    public Connecting(String username, String password) throws SQLException {
-        Connecting.username = username;
+    public Connecting(String login, String password) throws SQLException {
+        Connecting.login = login;
         Connecting.password = password;
         log.info("Opening connection to najlepszyzespol database");
-        conn = DriverManager.getConnection(url, Connecting.username, Connecting.password);
+        conn = DriverManager.getConnection(url, Connecting.login, Connecting.password);
     }
 
     public Connection getConn() {
@@ -90,7 +91,37 @@ public class Connecting {
         return categories;
     }
 
-    /*public void addAccount(int id, String username, String password, LocalDate creation_time){
+    public List<User> getUsers() {
+        List<User>users = new ArrayList<User>();
+        if (conn != null) {
+            Statement stmt = null;
+            try {
+                stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT * FROM USERS");
+                while (rs.next()) {
+                    User user = new User();
+                    user.setLogin(rs.getString("login"));
+                    user.setPassword(rs.getString("password"));
+                    user.setName(rs.getString("name"));
+                    user.setSurname(rs.getString("surname"));
+                    //user.setBirthDate(LocalDate.parse(rs.getString("birthdate")));
+                    //user.setAccountCreationDate(LocalDate.parse(rs.getString("accountcreationdate")));
+                    users.add(user);
+                }
+            } catch (SQLException e) {
+                log.error("Unable to get categories", e);
+            } finally {
+                if (stmt != null) {
+                    try { stmt.close();
+                    } catch (SQLException e) {
+                    }
+                }
+            }
+        }
+        return users;
+    }
+
+    /*public void addAccount(int id, String login, String password, LocalDate creation_time){
         Connecting.altertable
     }
     */
