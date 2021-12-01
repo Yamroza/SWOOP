d69493a7eb410ea3da713d59c54904bad84980fe
Aleksandra Jamróz <01158705@pw.edu.pl>
2021-12-01 23:38:04 +0100

    Removed useful comment

--- src/main/java/Database/Database.java
+++ src/main/java/Database/Database.java
@@ -12,12 +12,3 @@ public class Database {
         return users;
     }
 }
-/*
-    jeszcze nie wiem co to zwraca ale może się przydać
-    public ResultSet query(String query) throws SQLException
-    {
-        Statement stmt = con.createStatement();
-        return stmt.executeQuery(query);
-    }
-}
-*/
