abdbb891a10e705350e6afc0d767de61a01104e3
Gabriela Topczewska <01158963@pw.edu.pl>
2022-01-07 23:38:41 +0100

    rangeSlider added

--- src/main/java/GUI/MainScreen.java
+++ src/main/java/GUI/MainScreen.java
@@ -3,11 +3,15 @@ package GUI;
 import Classes.Offer;
 import Database.Categories;
 import Database.Offers;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ListView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.event.ActionEvent;
@@ -17,9 +21,20 @@ import org.controlsfx.control.CheckComboBox;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
 
 
 public class MainScreen {
+
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+
+    private NumberFormat format = NumberFormat.getInstance(Locale.FRANCE);
+
+    @FXML
+    RangeSlider range;
 
     @FXML
     ListView<Offer> offerList;
@@ -33,10 +48,17 @@ public class MainScreen {
 
 
     @FXML
-    private void ButtonClicked(ActionEvent e) throws IOException{
-        App.setRoot("offerScreen");
-        App.myStage.setScene(App.scene);
-        App.myStage.sizeToScene();
+    private void searchEnter(KeyEvent e) throws IOException, SQLException{
+        if(e.getCode() == KeyCode.ENTER) {
+            // wyszukiwanie
+        }
+    }
+
+    @FXML
+    private void rangeEnter(KeyEvent e) throws IOException, SQLException{
+        if(e.getCode() == KeyCode.ENTER) {
+            // wyszukiwanie
+        }
     }
 
     @FXML
@@ -62,10 +84,57 @@ public class MainScreen {
     }
 
     @FXML
-    private void initialize() throws SQLException {
+    private void initialize() throws SQLException, ParseException {
         categories.getItems().addAll(Categories.getCategoriesList());
         offerList.setItems(Offers.getNextTenOffers());
         offerList.setCellFactory(offerListView -> new OfferListElement());
+
+        rangeListen();
+
     }
 
+    @FXML
+    private void rangeListen() throws ParseException {
+
+            range.lowValueProperty().addListener(new ChangeListener<Number>() {
+                @Override
+                public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                    String low = String.valueOf(df.format(range.getLowValue()));
+                    fromTextField.setText(low);
+                }
+            });
+
+            range.highValueProperty().addListener(new ChangeListener<Number>() {
+                @Override
+                public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                    String high = String.valueOf(df.format(range.getHighValue()));
+                    toTextField.setText(high);
+                }
+            });
+
+        fromTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+            Number number = null;
+            try {
+                number = format.parse(newValue);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            double d = number.doubleValue();
+
+            range.setLowValue(d);
+        });
+
+        toTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+            Number number = null;
+            try {
+                number = format.parse(newValue);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            double d = number.doubleValue();
+
+            range.setHighValue(d);
+        });
+
+    }
 }
