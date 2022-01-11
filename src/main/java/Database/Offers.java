33bc7a0e869bb05f74b395a08180dc9400751bcd
Radosław Kostrzewski <01158754@pw.edu.pl>
2022-01-11 21:04:04 +0100

    Little gui change

--- src/main/java/Database/Offers.java
+++ src/main/java/Database/Offers.java
@@ -41,10 +41,10 @@ public class Offers {
         int status = rs.getInt("offer_status");
         offer.setStatus(status);
         offer.setLocalisation(rs.getString("localisation"));
-//        if(status == 1)
-//        {
-//            offer.setBuyer(rs.getString("buyer"));
-//        }
+        if(status == 1)
+        {
+            offer.setBuyer(rs.getString("buyer"));
+        }
         return offer;
     }
 
