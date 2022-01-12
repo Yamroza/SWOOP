5194a409a6b166d7428c9fdce7f590af612fe936
Aleksandra Jamróz <01158705@pw.edu.pl>
2022-01-12 11:35:12 +0100

    Offers list with pictures

--- src/main/java/GUI/MainScreen.java
+++ src/main/java/GUI/MainScreen.java
@@ -90,21 +90,7 @@ public class MainScreen {
     @FXML
     private void NewOfferClicked(ActionEvent e) throws IOException{
         App.setRoot("newOfferScreen");
-        /*InputStream stream = new FileInputStream("C://Users//oyamr//Documents//new_pap//star.jpg");
-        Image image = new Image(stream);
-        //Creating the image view
-        ImageView imageView = new ImageView();
-        //Setting image to the image view
-        imageView.setImage(image);
-        //Setting the image view parameters
-        imageView.setX(10);
-        imageView.setY(10);
-        imageView.setFitWidth(575);
-        imageView.setPreserveRatio(true);
-        //Setting the Scene object
-        Group root = new Group(imageView);
-        Scene scene = new Scene(root, 595, 370);*/
-        App.myStage.setScene(App.scene); //App.scene);
+        App.myStage.setScene(App.scene);
         App.myStage.sizeToScene();
     }
 
--- src/main/java/GUI/OfferListElement.java
+++ src/main/java/GUI/OfferListElement.java
@@ -4,10 +4,16 @@ import Classes.Offer;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ListCell;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import org.apache.commons.io.FileUtils;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class OfferListElement extends ListCell<Offer> {
 
@@ -22,6 +28,9 @@ public class OfferListElement extends ListCell<Offer> {
 
     @FXML
     Text itemExchange;
+
+    @FXML
+    ImageView itemPhoto;
 
     private FXMLLoader loader;
 
@@ -51,6 +60,7 @@ public class OfferListElement extends ListCell<Offer> {
                     e.printStackTrace();
                 }
             }
+
             itemCell.setVisible(true);
             itemName.setText(offer.getItemName());
             itemExchange.setVisible(false);
@@ -64,6 +74,22 @@ public class OfferListElement extends ListCell<Offer> {
                 itemPrice.setVisible(true);
                 itemPrice.setText(offer.getPrice() + "PLN");
             }
+            URL url = null;
+            try {
+                url = new URL(offer.getPhoto());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            File file = new File("work_image.jpg");
+            int connectionTimeout = 10 * 1000; // 10 sec
+            int readTimeout = 300 * 1000; // 3 min
+            try {
+                FileUtils.copyURLToFile(url, file, connectionTimeout, readTimeout);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Image image = new Image(file.toURI().toString());
+            itemPhoto.setImage(image);
         }
 
         setText(null);
