2f700ede7f4ed93d86854355c90d8b867e399524
Aleksandra Majewska <01158829@pw.edu.pl>
2022-01-27 21:00:30 +0100

    sequences in triggers

--- database_files/project_triggers.sql
+++ database_files/project_triggers.sql
@@ -1,21 +1,19 @@
+CREATE SEQUENCE comment_history_pk START WITH 1 maxvalue 2000;
+
+
 CREATE OR REPLACE TRIGGER on_change_comment_tg
 BEFORE UPDATE ON COMMENTS
 FOR EACH ROW
 DECLARE
     new_index NUMBER;
 BEGIN
-    SELECT max(c.COMMENT_CHANGE_ID) INTO new_index FROM COMMENTS_HISTORY c;
-    IF new_index IS NULL THEN
-        new_index := 1;
-    ELSE
-        new_index := new_index + 1;
-    END IF;
+    SELECT comment_history_pk.nextval INTO new_index FROM DUAL;
     INSERT INTO COMMENTS_HISTORY VALUES (new_index, :old.COMMENT_ID, :old.OFFER_ID,
                                          :old.USER_LOGIN, :old.COMMENT_TEXT,
                                          CURRENT_DATE);
 end;
 
-
+CREATE SEQUENCE offer_history_pk START WITH 1 maxvalue 2000;
 
 CREATE OR REPLACE TRIGGER on_change_offer_tg
 BEFORE UPDATE ON OFFERS
@@ -23,13 +21,7 @@ FOR EACH ROW
 DECLARE
     new_index NUMBER;
 BEGIN
-    SELECT max(o.OFFER_CHANGE_ID) INTO new_index FROM OFFERS_HISTORY o;
-    SELECT max(c.COMMENT_CHANGE_ID) INTO new_index FROM COMMENTS_HISTORY c;
-    IF new_index IS NULL THEN
-        new_index := 1;
-    ELSE
-        new_index := new_index + 1;
-    END IF;
+    SELECT offer_history_pk.nextval INTO new_index FROM DUAL;
     INSERT INTO OFFERS_HISTORY VALUES (new_index, :old.OFFER_ID, :old.NAME,
                                        :old.DESCRIPTION, :old.CATEGORY,
                                         :old.FOR_EXCHANGE, :old.FOR_SALE,
